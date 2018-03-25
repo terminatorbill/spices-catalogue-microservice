0c39b54d303941535d772aa4b4d2bc54712c533a
Bill Soumakis <scorpion-bill@hotmail.com>
2018-03-26 01:04:21 +0300

    [SP-3]: Added @Transactional in service

--- spices-application/runtime/src/main/java/com/spices/persistence/repository/CategoryRepositoryImpl.java
+++ spices-application/runtime/src/main/java/com/spices/persistence/repository/CategoryRepositoryImpl.java
@@ -1,11 +1,24 @@
 package com.spices.persistence.repository;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.persistence.EntityManager;
+
 import com.spices.domain.Category;
 
 public class CategoryRepositoryImpl implements CategoryRepository {
 
+    private final Provider<EntityManager> entityManagerProvider;
+
+    @Inject
+    CategoryRepositoryImpl(Provider<EntityManager> entityManagerProvider) {
+        this.entityManagerProvider = entityManagerProvider;
+    }
+
     @Override
     public void createCategory(Category category) {
+        EntityManager entityManager = entityManagerProvider.get();
+
 
     }
 }
--- spices-application/runtime/src/main/java/com/spices/service/CategoryServiceImpl.java
+++ spices-application/runtime/src/main/java/com/spices/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.spices.service;
 
+import com.google.inject.persist.Transactional;
 import com.spices.persistence.repository.CategoryRepository;
 import com.spices.domain.Category;
 
@@ -10,12 +11,13 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Inject
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+    CategoryServiceImpl(CategoryRepository categoryRepository) {
 
         this.categoryRepository = categoryRepository;
     }
 
     @Override
+    @Transactional
     public void createCategory(Category category) {
         categoryRepository.createCategory(category);
     }
