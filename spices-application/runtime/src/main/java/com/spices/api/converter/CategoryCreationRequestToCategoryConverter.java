907bec982412c35ab2ea28261c3ad1035ea946b8
Bill Soumakis <scorpion-bill@hotmail.com>
2018-03-28 00:12:50 +0300

    [SP-3]: Changed hamcrest-core to hamcrest-all and added unit tests for CategoryCreationRequestToCategoryConverter

--- spices-application/runtime/src/main/java/com/spices/api/converter/CategoryCreationRequestToCategoryConverter.java
+++ spices-application/runtime/src/main/java/com/spices/api/converter/CategoryCreationRequestToCategoryConverter.java
@@ -5,15 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.spices.api.dto.CategoryCreationRequestDto;
-import com.spices.api.dto.ImageDto;
-import com.spices.api.dto.MediaDto;
-import com.spices.api.dto.ProductDto;
-import com.spices.api.dto.VideoDto;
 import com.spices.domain.Category;
-import com.spices.domain.Image;
-import com.spices.domain.Media;
-import com.spices.domain.Product;
-import com.spices.domain.Video;
 
 public class CategoryCreationRequestToCategoryConverter {
 
@@ -23,49 +15,17 @@ public class CategoryCreationRequestToCategoryConverter {
             categoryCreationRequestDto.getParentCategoryId(),
             categoryCreationRequestDto.getName(),
             categoryCreationRequestDto.getDescription(),
-                Collections.emptyList(),
+            Collections.emptyList(),
             getSubCategories(categoryCreationRequestDto.getSubCategories())
         );
     }
 
     private List<Category> getSubCategories(List<CategoryCreationRequestDto> subCategories) {
+        if (subCategories == null || subCategories.isEmpty()) {
+            return Collections.emptyList();
+        }
         return subCategories.stream()
             .map(this::convert)
             .collect(Collectors.toList());
-    }
-
-    private static Product getProduct(ProductDto productDto) {
-        return new Product(
-            null,
-            productDto.getName(),
-            productDto.getDescription(),
-            productDto.getCategories(),
-            productDto.getPrice(),
-            getMedia(productDto.getMediaDto())
-        );
-    }
-
-    private static Media getMedia(MediaDto mediaDto) {
-        return new Media(getImages(mediaDto.getImages()), getVideos(mediaDto.getVideos()));
-    }
-
-    private static List<Image> getImages(List<ImageDto> imageDtos) {
-        return imageDtos.stream()
-            .map(CategoryCreationRequestToCategoryConverter::getImage)
-            .collect(Collectors.toList());
-    }
-
-    private static Image getImage(ImageDto imageDto) {
-        return new Image(null, imageDto.getUrl(), imageDto.getName(), imageDto.getFormat(), imageDto.getCaption());
-    }
-
-    private static List<Video> getVideos(List<VideoDto> videoDtos) {
-        return videoDtos.stream()
-            .map(CategoryCreationRequestToCategoryConverter::getVideo)
-            .collect(Collectors.toList());
-    }
-
-    private static Video getVideo(VideoDto videoDto) {
-        return new Video(null, videoDto.getUrl(), videoDto.getName(), videoDto.getFormat());
     }
 }
